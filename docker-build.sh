2a28b3443a50070bd252688ac579715fa26f9ea6
tonymmm1 <tony@mail.magnatox.com>
2020-04-24 13:50:05 -0500

    fixed docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #This script leverages .dockerignore files to help speed up the building process for each container and reduce waiting for docker build context
-PATH=$(pwd)
+root=$(pwd)
 git submodule init
 git submodule update --remote --recursive
 
 cd src/core/sql/migrations
 chmod +x merge.sh
 ./merge.sh
-cd $PATH
+cd $root
 
 docker build -t vmangos_build -f docker/build/Dockerfile . &&\ 
 	docker run -v $(pwd)/vmangos:/vmangos \
